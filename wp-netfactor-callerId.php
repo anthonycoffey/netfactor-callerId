94ff83ae3544b2e1c6b33eca52edef2a9a7f67b6
Anthony Coffey <coffey.j.anthony@gmail.com>
2018-11-16 17:51:08 -0600

    various updates, adding logic that changes company name and array that contains 'enabled dimensions'

--- wp-netfactor-callerId.php
+++ wp-netfactor-callerId.php
@@ -3,41 +3,44 @@
 Plugin Name: Netfactor CallerId for WordPress
 Description: This is a custom plugin that utilizes the VisitorTrack IP-Based API to target all dimensions using a prefixed CSS selector, and then inesrt the respective value into the targeted HTML element.
 Author: Anthony Coffey
-Version: 0.1
+Version: 0.2
 Author URI: https://coffeywebdev.com
 
 /**
- *
- * This function is triggered by the 'wp_head' action hook
- * more info: https://codex.wordpress.org/Function_Reference/wp_head
- *
- * Below I've used the 'Dimension' with a prefix of 'nf_' to create a CSS selector that looks like this:
- * .nf_companyId
- * .nf_companyName
- * .nf_websiteUrl
- *  and so on...
- *
- *  Anywhere these classes are used, the netfactor_callerId() function defined below will target
- *  all of the CSS selectors in the Dimension column listed on page 5 of the VisitorTrack IP-Based API Documentation
- *  and using jQuery will insert the respective value into the targeted HTML element.
- *
- *  For example:
- *
- *  Anywhere on the site, if there was an HTML element that looked like this:
- *  <span class="nf_companyId"></span>
- *
- *  On page load, this function will insert the respective value for companyId using jQuery .html() function,
- *  this will empty the node so be sure to use something like span above to avoid any content being erased accidentally.
- *
- */
+*
+* This function is triggered by the 'wp_head' action hook
+* more info: https://codex.wordpress.org/Function_Reference/wp_head
+*
+* Below I've used the 'Dimension' with a prefix of 'nf_' to create a CSS selector that looks like this:
+* .nf_companyId
+* .nf_companyName
+* .nf_websiteUrl
+*  and so on...
+*
+*  Anywhere these classes are used, the netfactor_callerId() function defined below will target
+*  all of the CSS selectors in the Dimension column listed on page 5 of the VisitorTrack IP-Based API Documentation
+*  and using jQuery will insert the respective value into the targeted HTML element.
+*
+*  For example:
+*
+*  Anywhere on the site, if there was an HTML element that looked like this:
+*  <span class="nf_companyId"></span>
+*
+*  On page load, this function will insert the respective value for companyId using jQuery .html() function,
+*  this will empty the node so be sure to use something like span above to avoid any content being erased accidentally.
+*
+*/
+
 add_action('wp_head', 'netfactor_callerId');
 function netfactor_callerId(){
+	// ENABLED DIMENSIONS
+	// add more strings to the array to enable support for more values
+	$enabled_dimensions = array('companyName');
+
 
 	// get IP address of visitor
 	if(function_exists('netfactor_get_user_ip')){
-		$ip_address = (netfactor_get_user_ip() <> '127.0.0.1') ? netfactor_get_user_ip() : '192.28.2.52';
-	} else {
-		$ip_address = '192.28.2.52';
+		$ip_address = netfactor_get_user_ip();
 	}
 
 	// define REST variables required for GET request
@@ -60,15 +63,24 @@ function netfactor_callerId(){
 			// create javascript wrapper
 			echo "<script>(function ($, window, document, undefined) {'use strict';$(function () {";
 			$body = json_decode(wp_remote_retrieve_body($get));
+
+			if($body->isp == false){
+				// do nothing special
+			} else {
+				// IF ISSSP FALSE, THEN DISPLAY "YOUR COMPANY" INSTEAD OF VALUE RETURNED FROM API
+				$body->companyName = "Your Company";
+			}
+
 			foreach ($body as $key => $value){
-				//echo "console.log('.{$key} - {$value}');";  // for debug only
 				/*
 				 * target each dimension, and autofill the value using jQuery function .html()
 				 * more info: http://api.jquery.com/html/
 				 *
 				 */
-				echo "jQuery('input.nf_{$key}').val('{$value}');";
-				echo "jQuery('.nf_{$key}:not(input)').html('{$value}');";
+					if(in_array($key, $enabled_dimensions)):
+						echo "jQuery('input.nf_{$key}').val('{$value}');";
+						echo "jQuery('.nf_{$key}:not(input)').html('{$value}');";
+					endif;
 			}
 			echo "});})(jQuery, window, document);</script>";
 			$autofill_html = ob_get_clean();
@@ -81,26 +93,30 @@ function netfactor_callerId(){
 
 
 /**
- * This function does the same thing as netfactor_callerId(), except it just prints out <span class="nf_DIMENSIONHERE">
- * for every Dimension listed on page 5 of the VisitorTrack IP-Based API Documentation
- *
- * This function is triggered by using the following shortcode: [netfactor_debug]
- *
- * This function is used to test the javascript that "auto-fills" all matched values
- */
+* This function does the same thing as netfactor_callerId(), except it just prints out <span class="nf_DIMENSIONHERE">
+* for every Dimension listed on page 5 of the VisitorTrack IP-Based API Documentation
+*
+* This function is triggered by using the following shortcode: [netfactor_debug]
+*
+* This function is used to test the javascript that "auto-fills" all matched values
+*
+*/
 add_shortcode('netfactor_debug','netfactor_make_spans');
 function netfactor_make_spans(){
+	// ENABLED DIMENSIONS
+	// add more strings to the array to enable support for more values
+	$enabled_dimensions = array('companyName');
+
 	// get IP address of visitor
 	if(function_exists('netfactor_get_user_ip')){
-		$ip_address = (netfactor_get_user_ip() <> '127.0.0.1') ? netfactor_get_user_ip() : '192.28.2.52';
-	} else {
-		$ip_address = '192.28.2.52';
+		$ip_address = netfactor_get_user_ip();
 	}
 
 	// define variables
 	$api_url = 'http://sleuth.visitor-track.com/Sleuth?ipAddress='.$ip_address;
 	$token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjamVmZmVyc0BuZXRmYWN0b3IuY29tIiwianRpIjoiYmRlN2M2NWYtM2U1ZS00N2MzLWJjYzktYzljM2IyNjU5YTk4IiwiZW1haWwiOiJjamVmZmVyc0BuZXRmYWN0b3IuY29tIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6ImNqZWZmZXJzQG5ldGZhY3Rvci5jb20iLCJleHAiOjE4NTc5Njk4NTksImlzcyI6Imh0dHA6Ly9zbGV1dGgudmlzaXRvci10cmFjay5jb20iLCJhdWQiOiJodHRwOi8vc2xldXRoLnZpc2l0b3ItdHJhY2suY29tIn0.GhmHjeAHrmctO-dDjRj3AK-eEmw2haiwukfW8SRBBUQ';
 	$bearer = 'Bearer';
+
 	// check to ensure this version of WordPress has wp_remote_get() function
 	if(function_exists('wp_remote_get')){
 		$get = wp_remote_get($api_url,array(
@@ -113,9 +129,21 @@ function netfactor_make_spans(){
 		if(function_exists('wp_remote_retrieve_body')){
 
 			$body = json_decode(wp_remote_retrieve_body($get));
+
+
+			if($body->isp == false){
+				var_dump($body);
+			} else {
+				// If "isp" is true, then change companyName to "Your Company"
+				$body->companyName = "Your Company";
+			}
+
+
 			foreach ($body as $key => $value){
-				echo "<span class='nf_{$key}' style='display: block;'></span>";
-				echo "<input class='nf_{$key}' value='' type='text'></input>";
+				if(in_array($key, $enabled_dimensions)) {
+					echo "<span class='nf_{$key}' style='display: block;'></span>";
+					echo "<input class='nf_{$key}' value='' type='text'>";
+				}
 			}
 
 		}
